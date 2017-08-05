2521b4dec40317592db9f7c8c38ee697be06845d
veay <bulaodehuiyi@163.com>
2017-08-05 14:48:26 +0800

    工具集合

---
+++ dynamic-profiles/src/main/java/com/lee/DynamicProfilesApplication.java
@@ -0,0 +1,12 @@
+package com.lee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamicProfilesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DynamicProfilesApplication.class, args);
+	}
+}
