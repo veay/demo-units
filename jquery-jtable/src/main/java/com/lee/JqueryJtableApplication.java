7be0f9aa393c63367f95fd4db0820e5ad89fedbf
veay <bulaodehuiyi@163.com>
2017-08-09 18:02:07 +0800

    jquery jtable

---
+++ jquery-jtable/src/main/java/com/lee/JqueryJtableApplication.java
@@ -0,0 +1,12 @@
+package com.lee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JqueryJtableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JqueryJtableApplication.class, args);
+	}
+}
